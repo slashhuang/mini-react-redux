4f180fcf1fde795b7eace95e2d61f9e98492478f
slashhuang <627284482@qq.com>
2017-02-07 14:24:57 +0800

    updating babel

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import { createStore } from 'redux';
 import React,{Component,PropTypes} from "react";
 import {Provider,connect} from "react-redux";
 import {render} from "react-dom";
+let test={a:{
+    b:1,
+    c:2
+}};
+let {c,b}=test.a;
+console.log(c,b)
 var reducer=function(state,action){
     return Object.assign({},state,action);
 };
@@ -19,18 +25,43 @@ let store = createStore(reducer,{"hello":"world"});
 class Container extends Component{
     constructor(props){
         super(props);
+        this.state={
+            a:0
+        }
     }
     componentDidMount(){
-       setTimeout(()=>this.props.action(),2000);
+       //setTimeout(()=>this.props.action(),2000);
+        console.log('====didmount0=>1===')
+            this.setState({
+                a:1
+            });
+        console.log(this.state.a)
+        setTimeout(()=>{this.setState({
+            a:2})
+            console.log('====timeout 1=>2===')
+            console.log(this.state.a)
+        }, 500 );
+
+    }
+    clickDom(){
+        "use strict";
+        console.log('====onClick 2=>3===');
+        this.state.a=4;
+        this.setState({a:3});
+         console.log(this.state.a)
+         this.state.a=5;
+        console.log(this.state.a)
     }
     render(){
+        console.log('render', this.state.a)
         return(
             <div>
-                <div>执行动作:{JSON.stringify(this.props.hint)}</div>
+                <div onClick={()=>this.clickDom()}>测试react1管理的事件</div>
                 <ul>{this.props.data.length?'结果':''}
                     {this.props.data.map((ele)=>{
                    return <li>{ele}</li>
                 })}</ul>
+                {this.state.a}
             </div>
         )
     }
